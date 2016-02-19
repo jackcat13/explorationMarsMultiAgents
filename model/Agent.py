6417b57bf14c65d137590a7fa82146ac4c1b4c30
jackCat13 <christophehenry11@gmail.com>
2016-02-19 09:35:46 +0100

    Agent movement on X

--- model/Agent.py
+++ model/Agent.py
@@ -1,11 +1,14 @@
 from Content import Content
+from Empty import Empty
 
 #Agent class which extends Content class. This class describes an Agent in the environment
 class Agent(Content):
 
-    def __init__(self, name):
+    def __init__(self, name, posX, posY):
         Content.__init__(self, name)
         self.next
+        self.posX = posX
+        self.posY = posY
 
     def next(self):
         return self.next
@@ -15,3 +18,27 @@ class Agent(Content):
 
     def terminate(self):
         del self
+
+    def move(self, directionX, directionY, environment):
+        self.moveX(directionX, environment)
+
+    def moveX(self, directionX, environment):
+        #If explorator goes on the left
+        if directionX == 0:
+            self.moveXLeft(directionX, environment)
+        #else if explorator goes on the right
+        else:
+            self.moveXRight(directionX, environment)
+
+    def moveXLeft(self, directionX, environment):
+        #If explorator is not on the left border and if the box on the left in the environment is empty
+        if self.posX > 0 & isinstance(environment[self.posX-1, self.posY], Empty):
+            #Then move to the left
+            environment[self.posX, self.posY] = Empty()
+            environment[self.posX-1, self.posY] = self
+
+    def moveXRight(self, directionX, environment):
+        #If explorator is not on the right border and if the box on the right in the environment is empty
+        if self.posX < environment.length & isinstance(environment[self.posX+1, self.posY], Empty):
+            environment[self.posX, self.posY] = Empty()
+            environment[self.posX+1, self.posY] = self
